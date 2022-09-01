4bce39d9eff4d51ffa5cd08620e3fee2ca93c4a8
dmnitprof <dmn.itprof@gmail.com>
2022-09-02 00:19:16 +0300

    edit app

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,34 +1,46 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
+
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import { useForm } from "react-hook-form";
 
 import styles from "./Login.module.scss";
-import { fetchAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+  const isAuth = useSelector(selectIsAuth);
   const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
-    setError,
+    // setError,
     formState: { errors, isValid },
   } = useForm({
     defaultValues: {
-      email: "test@test.ru",
-      password: "123",
+      email: "",
+      password: "",
     },
     mode: "onChange",
   });
 
-  const onSubmit = (values) => {
-    dispatch(fetchAuth(values));
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+    console.log(data.payload);
+    // if (!data.payload) {
+    //   return alert("Не удалось авторизоваться!");
+    // }
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+    }
   };
 
-  console.log(errors, isValid);
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <Paper classes={{ root: styles.root }}>
@@ -39,6 +51,7 @@ export const Login = () => {
         <TextField
           className={styles.field}
           label="E-Mail"
+          type="email"
           error={Boolean(errors.email?.message)}
           helperText={errors.email?.message}
           {...register("email", { required: "Укажите почту" })}
@@ -47,12 +60,19 @@ export const Login = () => {
         <TextField
           className={styles.field}
           label="Пароль"
+          type="password"
           error={Boolean(errors.password?.message)}
           helperText={errors.password?.message}
           {...register("password", { required: "Укажите пароль" })}
           fullWidth
         />
-        <Button type="submit" size="large" variant="contained" fullWidth>
+        <Button
+          disabled={!isValid}
+          type="submit"
+          size="large"
+          variant="contained"
+          fullWidth
+        >
           Войти
         </Button>
       </form>
--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,13 +1,23 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../axios";
 
-export const fetchAuth = createAsyncThunk(
-  "auth/fetchUserData",
+export const fetchAuth = createAsyncThunk("auth/fetchAuth", async (params) => {
+  const { data } = await axios.post("/auth/login", params);
+  return data;
+});
+
+export const fetchRegister = createAsyncThunk(
+  "auth/fetchRegister",
   async (params) => {
-    const { data } = await axios.post("/auth/login", params);
+    const { data } = await axios.post("/auth/register", params);
     return data;
   }
 );
+
+export const fetchAuthMe = createAsyncThunk("auth/fetchAuthMe", async () => {
+  const { data } = await axios.get("/auth/me");
+  return data;
+});
 
 const initialState = {
   data: null,
@@ -17,7 +27,24 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  reducers: {
+    logout: (state) => {
+      state.data = null;
+    },
+  },
   extraReducers: {
+    [fetchRegister.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+    [fetchRegister.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchRegister.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
     [fetchAuth.pending]: (state) => {
       state.status = "loading";
       state.data = null;
@@ -30,7 +57,23 @@ const authSlice = createSlice({
       state.status = "error";
       state.data = null;
     },
+    [fetchAuthMe.pending]: (state) => {
+      state.status = "loading";
+      state.data = null;
+    },
+    [fetchAuthMe.fulfilled]: (state, action) => {
+      state.status = "loaded";
+      state.data = action.payload;
+    },
+    [fetchAuthMe.rejected]: (state) => {
+      state.status = "error";
+      state.data = null;
+    },
   },
 });
 
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
 export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
